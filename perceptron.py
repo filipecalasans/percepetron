20c7f50af7855809384d93f07e4412c56a7d8f9e
Filipe Calasans <filipe.calasans@gmail.com>
2017-12-12 23:10:18 -0500

    formatting

--- perceptron.py
+++ perceptron.py
@@ -4,12 +4,12 @@ import random
 
 class Percepetron:
     
-  def __init__(self, input_size):
+  def __init__(self, input_size, eta=0.1, threshold=1e-3):
     # Generate randomly
     self.w = np.random.uniform([-0.5, 0.5, input_size+1])
     self.fnet = self.f
-    self.etha = 0.01
-    self.threshold = 1e-3
+    self.eta = eta
+    self.threshold = threshold
     self.sqerror = 0
 
   def init_wieghts(self):
@@ -18,11 +18,11 @@ class Percepetron:
   def train(self, dataset):
     
     # Training Equations
-    # w_1(t+1) = w_1(t) - etha * dE2/dW_1
-    # w_2(t+1) = w_2(t) - etha * dE2/dW_2
-    # theta(t+1) = theta(t) - etha * dE2/dtheta
+    # w_1(t+1) = w_1(t) - eta * dE2/dW_1
+    # w_2(t+1) = w_2(t) - eta * dE2/dW_2
+    # theta(t+1) = theta(t) - eta * dE2/dtheta
 
-    print("Training")
+    print("Training ...")
 
     # dataset = [ [x1_1, x1_2, ..., x1_n, y1],
     #             [x2_1, x2_2, ..., x2_n, y2], 
@@ -39,10 +39,12 @@ class Percepetron:
       
       print("############################")
       print("Error: {}, Threshold: {}".format(self.sqerror/n, self.threshold))
-      print(self)
+      # print(self)
 
       if (self.sqerror/n) < self.threshold:
         break
+
+    print("Training Done...")
 
   def apply_net_function(self, dataset):
     
@@ -51,46 +53,47 @@ class Percepetron:
     # print("# col: {}".format(col))
 
     # [x1, x2, ..., xn, 1]
-    x = np.append(dataset[-(col-1):],1)
+    x = np.append(dataset[:(col-1)], 1)
     y = dataset[(col-1):]
 
     net = np.dot(x, self.w)
 
-    # print("X = {}m y = {}".format(x, y))
+    # print("X = {}, y = {}".format(x, y))
 
     y_o = self.fnet(net)
     error = y - y_o
     
     self.sqerror += error**2
     
-    print("Error: {}".format(error))
+    # print("Error: {}".format(error))
 
     # print("Y:{}, Yo: {}, error = {}, error^2:{} ".format(y, y_o, error, self.sqerror))
 
-    # deltas = [2*error[0] - x_i for x_i in x]
-
-    dE2 = error * -x
+    dE2 = 2 * error * (-x)
     
     # print("dE2: {}".format(dE2))
     # print("W(t): {}", self.w)
     
-    self.w -= (self.etha * dE2)
+    self.w -= (self.eta * dE2)
 
     # print("W(t+1): {}", self.w)
     # print("sqerror {}".format(self.sqerror))
     # print("##################################################################")
 
   def f(self, net):
-    if net >= 0.5: return 1.0
+    if net >= 0.5: return 1
     
-    return 0.0
+    return 0
 
+  def test(self, x):
+    y = np.dot(self.w, np.append(x,1))
+    return self.fnet(y)
 
   def __str__(self):
     return 'Weights: {}'.format(self.w)
 
 
-def test(filename):
+def train_and_test_and(filename):
 
   dataset = np.loadtxt(open(filename, "rb"), delimiter=" ")
   
@@ -99,6 +102,21 @@ def test(filename):
   p = Percepetron(2)
   p.train(dataset)
 
-  print(p)
+  a = 0
+  b = 0
+  print("{} AND {}: {}".format(a,b, p.test(np.array([a, b]))))
 
-test("AND.dat")
+  a = 0
+  b = 1
+  print("{} AND {}: {}".format(a,b, p.test(np.array([a, b]))))
+
+  a = 1
+  b = 0
+  print("{} AND {}: {}".format(a,b, p.test(np.array([a, b]))))
+
+  a = 1
+  b = 1
+  print("{} AND {}: {}".format(a,b, p.test(np.array([a, b]))))
+
+if __name__ == "__main__":
+  train_and_test_and("AND.dat")
